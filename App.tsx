db4ef07d84d1b73abbc051dee1484b26a1d67317
Jenny Melnychenko <melnichenkojenny@gmail.com>
2024-03-27 16:39:22 +0200

    init project

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, ScrollView, Text } from "react-native";
+
+export const App = () => (
+  <SafeAreaView>
+    <ScrollView>
+      <Text>{"Hello Rizna team!!!"}</Text>
+    </ScrollView>
+  </SafeAreaView>
+);
+
+export default App;
